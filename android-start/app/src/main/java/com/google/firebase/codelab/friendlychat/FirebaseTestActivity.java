9edc25e606e661a7fa7aaa76ccfc95425b5d866c
ohdoking <ohdoking@gmail.com>
2017-02-23 01:56:17 +0900

    add Product database에 추가 및 가져오기

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/FirebaseTestActivity.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/FirebaseTestActivity.java
@@ -2,14 +2,28 @@ package com.google.firebase.codelab.friendlychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.codelab.friendlychat.dto.Product;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseTestActivity extends AppCompatActivity {
 
     private DatabaseReference mFirebaseDatabaseReference;
+    private Button sendBtn;
+    private Button getBtn;
+    private EditText productName;
+    private TextView productResult;
+
+    private DatabaseReference dr;
 
     public static final String SOMETHINGS = "test/somethings";
 
@@ -18,17 +32,55 @@ public class FirebaseTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_test);
 
+        sendBtn = (Button) findViewById(R.id.sendInfo);
+        getBtn = (Button) findViewById(R.id.getInfo);
+        productName = (EditText) findViewById(R.id.productInfo);
+        productResult = (TextView) findViewById(R.id.productResult);
+
         mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        DatabaseReference dr = mFirebaseDatabaseReference.child(SOMETHINGS);
+        dr = mFirebaseDatabaseReference.child(SOMETHINGS);
 
-        Product product = new Product();
-        product.setId("a");
-        product.setName("apple");
-        product.setPrice(1000);
-        product.setType("fruits");
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Product product = new Product();
+                product.setId("b");
+                product.setName(productName.getText().toString());
+                product.setPrice(1000);
+                product.setType("fruits");
 
-        dr.setValue(product);
+                dr.child("product").setValue(product);
+            }
+        });
+
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference productInfo = dr.child("product");
+
+                productInfo.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Product product = dataSnapshot.getValue(Product.class);
+                        Log.i("ohdoking","!!");
+                        productResult.setText(product.getName());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        System.out.println("The read failed: " + databaseError.getCode());
+                    }
+                });
+
+
+
+            }
+        });
+
+
+
+
 
     }
 }
